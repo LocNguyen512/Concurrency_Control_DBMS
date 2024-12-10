282e73dd022e6c20345f40c7117c2732eb2a2d05
Myy1112 <120002840+Myy1112@users.noreply.github.com>
2024-12-10 19:39:31 +0700

    demo tranh chap

---
+++ Demo/TH2B.sql
@@ -0,0 +1,34 @@
+use QLDPKS
+go
+-- Thiết lập mức độ cô lập là SERIALIZABLE
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
+BEGIN TRANSACTION;
+    -- Nhân viên B đọc và khóa bản ghi với XLOCK
+    SELECT max_num
+    FROM Room WITH (XLOCK)
+    WHERE type = 'Deluxe';
+
+ 
+    -- Giảm số lượng phòng trống
+    UPDATE Room
+    SET max_num = max_num - 1
+    WHERE type = 'Deluxe';
+
+	DECLARE @current_available_B INT;
+    SELECT @current_available_B = max_num
+    FROM Room WITH (XLOCK)
+    WHERE type = 'Deluxe';
+
+    -- In ra kết quả
+    PRINT N'Nhân viên B: Cập nhật thành công. Số lượng phòng hiện tại là ' + CAST(@current_available_B  AS NVARCHAR);
+
+    -- Hoàn tất giao dịch
+    COMMIT TRANSACTION;
+
+END TRY
+BEGIN CATCH
+    -- Xử lý lỗi: Rollback giao dịch
+    ROLLBACK TRANSACTION;
+    PRINT ERROR_MESSAGE();
+END CATCH;
