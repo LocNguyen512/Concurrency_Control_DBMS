98387d70df7ea5cb9272c036e640a0b1567fc869
Vy <lnyvy22@clc.fitus.edu.vn>
2024-12-10 18:48:47 +0700

    Thêm booking_time trong Room và thêm N trước kí tự UTF8.sql

--- ScriptCreateDatabase.sql
+++ ScriptCreateDatabase.sql
@@ -34,6 +34,7 @@ create table Room (
     max_num int,
     price float,
     status nvarchar(50) -- Trạng thái của phòng (ví dụ: Trống, Đã đặt, Đang dọn phòng, ...)
+    booking_time datetime;
 )
 go
 
@@ -60,47 +61,4 @@ go
 -- Tạo bảng UserDetail
 create table UserDetail (
     booking_id int references Booking (id),
-    room_id int references Room (id),
-    customer_id int references Users (id),
-    primary key (booking_id, room_id, customer_id)
-)
-go
-
--- Tạo bảng Category
-create table Category (
-    id int primary key identity(1,1),
-    name nvarchar(50)
-)
-go
-
--- Tạo bảng Product
-create table Product (
-    id int primary key identity(1,1),
-    category_id int references Category (id),
-    title nvarchar(150),
-    thumbnail nvarchar(500),
-    description ntext,
-    price float,
-    amount int
-)
-go
-
--- Tạo bảng Services
-create table Services (
-    id int primary key identity(1,1),
-    booking_id int references Booking (id),
-    customer_id int references Users (id),
-    product_id int references Product (id),
-    price float,
-    amount int,
-    buy_date datetime
-)
-go
-
-
-INSERT INTO Room (room_no, status)
-VALUES 
-    ('101', 'Trống'),  -- Phòng 101 là trống
-    ('102', 'Đã đặt'),  -- Phòng 102 đã được đặt
-    ('103', 'Trống'),  -- Phòng 103 là trống
-    ('104', 'Đã đặt');  -- Phòng 104 đã được đặt
+    room_id int rN'Đã đặt');  -- Phòng 104 đã được đặt
