928267618677db190e47d3b84cf72a1dff545d48
Vy <lnyvy22@clc.fitus.edu.vn>
2024-12-10 17:20:16 +0700

    Add files via upload

---
+++ Demo/SQLA_3.sql
@@ -0,0 +1,33 @@
+﻿
+use QLDPKS
+go
+
+-- Giao dịch của khách hàng A
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+BEGIN TRANSACTION;
+
+-- Khách hàng A đọc trạng thái phòng 103
+SELECT *
+FROM Room WITH (ROWLOCK) -- Đặt share lock
+WHERE room_no = '103';
+
+--Khách hàng A tiến hành đặt phòng 103
+-- Đặt trạng thái phòng 103 tạm thời là "Đã đặt"
+UPDATE Room
+SET status = N'Đã đặt', booking_time = GETDATE()
+WHERE room_no = '103' AND status = N'Trống'
+
+--Giao tác A kiểm tra lại trạng thái phòng hiện tại
+SELECT*
+from room
+where room_no = '103'
+
+-- Tiến hành giao dịch thanh toán, nhưng trạng thái phòng sẽ chưa được "Đã đặt" vĩnh viễn nếu chưa commit
+--Nếu khách hàng A hủy giao dịch, roll back
+ROLLBACK 
+-- Nếu khách hàng A hoàn tất giao dịch, commit
+--COMMIT;
+
+
+
+
---
+++ Demo/SQLB_3.sql
@@ -0,0 +1,21 @@
+﻿use QLDPKS
+go
+
+-- Giao dịch của khách hàng B
+--Đặt mức cô lập cho giao tác
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+BEGIN TRANSACTION;
+
+SELECT * 
+FROM Room 
+WHERE room_no = '103' 
+
+--Nếu khách hàng A rollback thì B có thể tiến hành đặt phòng
+-- Khách hàng B Tiến hành đặt phòng 103
+UPDATE Room
+SET status = N'Đã đặt', booking_time = GETDATE()
+WHERE room_no = '103' AND status = N'Trống';
+
+--Giao tác của B được commit nếu hoàn thành thanh toán
+COMMIT
+
