c8a6d9464777b9b101b3ae104ea065f10bd586bf
huyen12112k4 <128444657+huyen12112k4@users.noreply.github.com>
2024-12-10 10:42:59 +0700

    Update Demo

--- Demo/SQLA_1.sql
+++ Demo/SQLA_1.sql
@@ -1,74 +1,37 @@
 ﻿use QLDPKS
 go
 
--- Đặt mức cô lập giao dịch là Read Committed
-SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
-
--- Giao dịch 1: Khách A
-BEGIN TRANSACTION;  -- Bắt đầu giao dịch
-
--- Khách A đọc thông tin các phòng còn trống
-SELECT * 
-FROM Room r
-WITH (UPDLOCK)  -- Khóa chia sẻ (Share Lock) trong khi đọc
-WHERE r.status = N'Trống'
-
-
--- Hệ thống đặt khóa XLOCK lên dữ liệu trong khi chờ khách A hoàn tất thủ tục
-SELECT * 
-FROM Room 
-WITH (XLOCK)  -- Khóa chia sẻ (Share Lock) trong khi đọc
-WHERE room_no = '101' 
-AND status = N'Trống'
-
--- Khách A đọc thông tin các phòng còn trống
-if (exists(
-SELECT 1 
-FROM Room r  -- Khóa chia sẻ (Share Lock) trong khi đọc
-WHERE r.status = N'Trống'
-AND room_no = '101' ))
-BEGIN
-UPDATE Room 
--- Khóa độc quyền (Exclusive Lock)
-SET status = N'Đã đặt'
-WHERE room_no = '101' 
-AND status = N'Trống';  -- Phòng phải còn trống để đặt
-END
-
--- Cam kết giao dịch
-COMMIT TRANSACTION;
-
-
-
 -- Đặt mức cô lập giao dịch là Repeatable Read
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 
--- Giao dịch 1: Khách A
-BEGIN TRANSACTION;  -- Bắt đầu giao dịch
+-- Giao dịch A
+BEGIN TRANSACTION;
 
--- Khách A đọc thông tin các phòng còn trống
-SELECT * 
-FROM Room r
-WITH (UPDLOCK)  -- Khóa chia sẻ (Share Lock) trong khi đọc
-WHERE r.status = N'Trống'
+-- Khách hàng yêu cầu đọc số lượng những buffet hải sản còn lại
+SELECT *
+FROM Product
+WHERE id = 2 -- ID buffet hải sản
+    AND amount >= 1 -- Đảm bảo còn ít nhất 1 suất buffet
+-- A tiến hành hoàn thành thủ tục trên website
 
-
--- Khách A đọc thông tin các phòng còn trống
-if (exists(
-SELECT 1 
-FROM Room r  -- Khóa chia sẻ (Share Lock) trong khi đọc
-WHERE r.status = N'Trống'
-AND room_no = '104' ))
+-- Hệ thống kiểm tra lại số lượng buffet để chắc chắn còn suất
+IF EXISTS (
+    SELECT *
+    FROM Product
+    WHERE id = 2 -- ID buffet hải sản
+      AND amount >= 1 -- Đảm bảo còn ít nhất 1 suất buffet
+)
 BEGIN
-UPDATE Room 
--- Khóa độc quyền (Exclusive Lock)
-SET status = N'Đã đặt'
-WHERE room_no = '104' 
-AND status = N'Trống';  -- Phòng phải còn trống để đặt
+    -- Cập nhật số lượng buffet
+    UPDATE Product
+    SET amount = amount - 1
+	WHERE id = 2
+    PRINT N'Khách hàng A đã đặt buffet thành công.';
 END
--- Hệ thống cập nhật lại trạng thái phòng và kết thúc giao tác
-
+ELSE
+BEGIN
+    PRINT N'Không đủ số lượng buffet cho khách hàng A.';
+END;
 
 -- Cam kết giao dịch
 COMMIT TRANSACTION;
-
--- Demo/SQLB_1.sql
+++ Demo/SQLB_1.sql
@@ -1,17 +1,18 @@
 ﻿use QLDPKS
 go
 
--- Đặt mức cô lập giao dịch là Read Committed
+-- Đặt mức cô lập giao dịch là Repeatable Read
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
--- Giao dịch 2: Khách B
-BEGIN TRANSACTION;  -- Bắt đầu giao dịch
 
+-- Giao dịch B
+BEGIN TRANSACTION;
+    -- Yêu cầu cập nhật số lượng buffet
+    UPDATE Product
+    SET amount = amount - 1
+    WHERE id = 2
+      AND amount >= 1; -- Đảm bảo cập nhật chỉ khi còn đủ số lượng
 
-UPDATE Room 
--- Khóa độc quyền (Exclusive Lock)
-SET status = N'Đã đặt'
-WHERE room_no = '104' 
-AND status = N'Trống';  -- Phòng phải còn trống để đặt
+    PRINT 'Khách hàng B đã đặt buffet thành công.';
 
-
+-- Cam kết giao dịch
 COMMIT TRANSACTION;
